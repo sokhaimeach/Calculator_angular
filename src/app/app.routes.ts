92d82ef01a61c0b746ad6b0454e455ee09cb1c16
Meach Sokhai <dshhshh750@gmail.com>
2025-07-02 21:22:35 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Calculator } from './calculator/calculator';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'calculator', pathMatch: 'full'},
+    {path: 'calculator', component: Calculator}
+];
