69d7c48cb8ed7b88febd480f7285a06ed516c715
Meach Sokhai <dshhshh750@gmail.com>
2025-07-03 22:43:56 +0700

    update more

--- src/app/calculator/calculator.ts
+++ src/app/calculator/calculator.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calculator.html',
   styleUrl: './calculator.css'
 })
 export class Calculator {
+  combineNum: string = '';
+  outputNum: string = '';
 
+  getInput(number: string){
+    this.combineNum += number;
+    this.outputNum = this.combineNum;
+  }
+
+  calculate() {
+    this.combineNum = eval(this.combineNum);
+    this.outputNum = this.combineNum;
+  }
 }
