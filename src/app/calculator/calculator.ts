54cf6ad917d13922ccbf71bdaaf09789f0384fa4
Meach Sokhai <dshhshh750@gmail.com>
2025-07-03 23:27:28 +0700

    update operator

--- src/app/calculator/calculator.ts
+++ src/app/calculator/calculator.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -7,17 +7,49 @@ import { Component } from '@angular/core';
   templateUrl: './calculator.html',
   styleUrl: './calculator.css'
 })
-export class Calculator {
+export class Calculator implements OnInit {
   combineNum: string = '';
   outputNum: string = '';
+
+  ngOnInit(): void {
+    // this.liveOutput();
+  }
 
   getInput(number: string){
     this.combineNum += number;
     this.outputNum = this.combineNum;
+    this.liveOutput();
   }
 
   calculate() {
     this.combineNum = eval(this.combineNum);
     this.outputNum = this.combineNum;
+  }
+
+  reset() {
+    this.combineNum = '';
+    this.outputNum = '';
+  }
+
+  delete() {
+    if (this.combineNum == '') return;
+    const deleteNum = this.combineNum.split("");
+    this.combineNum = '';
+    for(let i = 0; i < deleteNum.length - 1; i++){
+      this.combineNum += deleteNum[i];
+    }
+    this.outputNum = this.combineNum;
+  }
+
+  liveOutput() {
+    const shownum = this.combineNum.split('');
+    this.outputNum = '';
+    for(let i = 0; i< shownum.length; i++){
+      if(shownum[i] == '*')
+        this.outputNum += '×';
+      else if(shownum[i] == '/')
+        this.outputNum += '÷';
+      else this.outputNum += shownum[i];
+    }
   }
 }
